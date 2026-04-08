7ced20b330f59bf00acffeff06d12fa0fc0e12ac
Gayathri Rangu <gayathri.devanandam@gmail.com>
2026-04-08 23:47:40 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #brute force wat: sort them off and groupt them if they are exactly the same
+        sortedanagram={}
+        sortedlists=[sorted(s) for s in strs]
+        
+        for i,j in zip(strs, sortedlists):
+            sortedanagram[j].append(i)
+        
+
+
+        
