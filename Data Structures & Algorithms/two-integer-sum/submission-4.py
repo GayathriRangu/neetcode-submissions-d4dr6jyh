8d89e619f3f5a0f5ae6839d17bad6fd8c01936bb
Gayathri Rangu <gayathri.devanandam@gmail.com>
2026-04-08 22:10:04 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        newdict={}
+        for i in range(len(nums)):
+            key=nums[i]
+            val=i
+            newdict[key]=val
+        for (i,j) in newdict.items():
+            comkey=target-i
+            newindex=newdict[comkey]
+            if comkey in newdict and j!=newindex:
+                return [j, newdict[target-i]] 
+        
+
+        
